10010e2601cf5594d1e843026edbc13bb21dbe17
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-04-23 12:36:43 +0200

    WHyNEC2

--- components/ReMembers.js
+++ components/ReMembers.js
@@ -11,13 +11,15 @@ function ReMembers() {
       <div className="flex justify-between w-full h-[550px] bg-[#2F4858] mt-20 px-10">
         <div className="pt-28 flex flex-wrap gap-4 relative w-[500px]">
           <h4 className="text-3xl font-semibold text-white">
+            <span className="border-b-4 border-orange-500">
             WHY <br></br> NEC?
+            </span>
           </h4>
           <div>
-            <Image src={Why} height={350} width={350} />
+            <Image src={Why} height={300} width={300} />
           </div>
-          <div className="bg-[#2F4858] opacity-80 h-20 border absolute top-48 p-2 text-white mt-4">
-            <p className="text-sm">
+          <div className="bg-[#2F4858] opacity-90 h-auto w-[320px] absolute top-48 p-2 text-white mt-12">
+            <p className="text-sm z-96">
               Lorem ipsum dolor sit amet, consectetur adipiscing
               elit.Aliquam magna metus, elementum vitae sem eget,
               iaculis varius mi. Donec vel elementum arcu.
@@ -26,11 +28,11 @@ function ReMembers() {
             </p>
           </div>
         </div>
-        <div className="grid gap-4 mt-4 mb-4">
-          <Stack />
-          <Stack />
-          <Stack />
-          <Stack />
+        <div className="grid mt-16 mb-4 ">
+          <Stack title="Availability" />
+          <Stack title="Client Focus" />
+          <Stack title="Goal Driven" />
+          <Stack title="Availability" />
         </div>
       </div>
     </>
--- components/Stack.js
+++ components/Stack.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Stack() {
-  return (
-    <div className='flex border border-solid text-center align-center items-center px-4 py-21'>
-        <h4>Availability</h4>
-        <p>lorem12</p>
-    </div>
-  )
+function Stack({title}) {
+    return (
+        <div className='border border-solid px-4 w-[400px] h-10 overflow-hidden hover:h-[300px] bg-white text-[#EE9408] duration-500 hover:text-white hover:bg-[#EE9408]'>
+            <h4 className='text-3xl text-center pt-1 duration-500 hover:text-left'>{title}</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis placerat eros quis risus pretium aliquet. Phasellus posuere eros ultricies, sagittis sapien at, tristique sem. Vestibulum ut lacinia leo.</p>
+        </div>
+    )
 }
 
 export default Stack
