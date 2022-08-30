ac93f0b4d35382d58dc2f4dc9aacc8d7da81df81
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-08-30 11:12:37 -0700

    more levels

--- components/Levels.js
+++ components/Levels.js
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 
 function Levels() {
   return (
-    <div className="flex gap-10 bg-black-600 justify-center items-center p-14">
+    <div className="flex gap-10 bg-black justify-center items-center p-14 text-white">
       <div className="grid place-items-center text-center">
         <Typography variant="h5" gutterBottom>
           300+
