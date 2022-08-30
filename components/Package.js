002613f2d98abfbabe42b6eb393c90203b7e12c1
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-08-30 10:41:29 -0700

    packages

---
+++ components/Package.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+
+function createData(lessons, price) {
+  return { lessons, price };
+}
+
+const rows = [
+  createData(159, 6.0),
+  createData(237, 9.0),
+  createData(262, 16.0),
+  createData(305, 3.7),
+  createData(356, 16.0),
+];''
+
+export default function Package({grade}) {
+  return (
+    <>
+    
+    <TableContainer component={Paper} sx={{minWidth:280, width:300}}>
+      <Typography variant="subtitle1" gutterBottom>
+          {grade}
+    </Typography>
+      <Table aria-label="Grade B table" >
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">
+            <Typography variant="caption" gutterBottom>
+          Lessons
+    </Typography>
+              </TableCell>
+              <TableCell align="center">
+            <Typography variant="caption" gutterBottom>
+          Prices
+    </Typography>
+              </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">{row.lessons}</TableCell>
+              <TableCell align="center">{row.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+    
+  );
+}
--- components/Trending.js
+++ components/Trending.js
@@ -1,24 +1,23 @@
 import React from "react";
-import Sector from "./Sector";
+import Package from "./Package";
 import Typography from '@mui/material/Typography';
 
 
 function Trending() {
   return (
     <>
-      <div className="py-20 text-center flex justify-center align-center m-auto">
+      <div className="px-10 py-20 text-center flex justify-center align-center m-auto">
         <div>
-        <Typography variant="h6" gutterBottom>
-          h6. Heading
+        <Typography variant="subtitle2" gutterBottom>
+          choose from our
         </Typography>
-        <Typography variant="h4" gutterBottom>
-        h4. Heading
+        <Typography variant="h5" gutterBottom>
+        Packages
       </Typography>
-          <div className="flex justify-center flex-wrap gap-12 pt-10">
-            <Sector title="FOOD & NUTRITION" />
-            <Sector title="AGRICULTURE" />
-            <Sector title="AGRICULTURE" />
-            <Sector title="AGRICULTURE" />
+          <div className="flex justify-between flex-wrap gap-12 pt-10">
+            <Package grade="Grade B"/>
+            <Package grade="Grade C"/>
+            <Package grade="Grade D"/>
           </div>
         </div>
       </div>
