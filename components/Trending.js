6905c471da53a00c2cea013a901d634ebe864269
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-09-02 17:56:40 -0700

    ourpackagestitle

--- components/Trending.js
+++ components/Trending.js
@@ -1,30 +1,28 @@
 import React from "react";
 import Package from "./Package";
-import Typography from '@mui/material/Typography';
-
+import Typography from "@mui/material/Typography";
 
 function Trending() {
   return (
     <>
       <div className="px-10 py-20 text-center flex justify-center align-center m-auto">
         <div>
-        <Typography variant="body2" gutterBottom>
-          choose from our
-        </Typography>
-        <Typography variant="h4" gutterBottom>
-        Packages
-      </Typography>
+          <div className="grid gap-2 pb-4">
+            <Typography variant="body2" gutterBottom>
+              choose from our
+            </Typography>
+            <Typography variant="h4" gutterBottom>
+              Packages
+            </Typography>
+          </div>
           <div className="flex justify-center flex-wrap gap-12 pt-10">
-            <Package grade="Grade B"/>
-            <Package grade="Grade C"/>
-            <Package grade="Grade D"/>
+            <Package grade="Grade B" />
+            <Package grade="Grade C" />
+            <Package grade="Grade D" />
           </div>
         </div>
       </div>
-      <style jsx>
-        {`
-        `}
-      </style>
+      <style jsx>{``}</style>
     </>
   );
 }
