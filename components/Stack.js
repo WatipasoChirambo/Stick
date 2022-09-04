c85f1e03368826834ff5d4acd1d3735b37ba5023
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-09-04 09:08:52 -0700

    colors

--- components/Stack.js
+++ components/Stack.js
@@ -5,7 +5,7 @@ import { FaStar } from "react-icons/fa";
 
 function Stack({ title }) {
   return (
-    <div className="p-2 rounded-md h-40 grid place-items-center bg-gray-500">
+    <div className="p-2 rounded-md h-40 grid place-items-center bg-black">
         <div>
           <Typography variant="subtitle1" gutterBottom>
             h5. Heading
