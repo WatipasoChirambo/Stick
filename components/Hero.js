ff066bcc1f539f04d14eb3a56af6663ca6c11291
Watipaso RattleStack Chirambo <76850781+WatipasoChirambo@users.noreply.github.com>
2022-09-04 09:44:34 -0700

    boy

--- components/Hero.js
+++ components/Hero.js
@@ -3,17 +3,14 @@ import { GiDrumKit, GiSaxophone, GiMusicalKeyboard } from "react-icons/gi";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-export const phone = 265999958336
+export const phone = 265999958336;
 
 function Hero() {
-
-  
-
   return (
     <>
       <div className="flex justify-center text-center wrapper px-10 items-center h-screen">
-        <div className="flex justify-center text-white">
-          <div className="lg:w-[70%]">
+        <div className="flex justify-center text-white ">
+          <div className=" lg:w-[70%]">
             <Typography variant="h2" gutterBottom>
               <span className="text-[#BFA5A4]">Learn</span> From The{" "}
               <span className="text-[#DC003E]">Best</span>
@@ -35,14 +32,23 @@ function Hero() {
         {`
           .wrapper {
             background: url("hero.jpg") center;
-            background-color: rgba(
+             background-color: rgba(
+              5,
               0,
-              0,
-              80,
+              75,
               0.8
-            ); /* use rgba for fine adjustments */
+            ); 
             background-blend-mode: multiply;
             background-size: cover;
+          }
+          ,
+          .deep {
+            background: radial-gradient(
+              circle,
+              rgba(0, 0, 0, 1) 0%,
+              rgba(0, 0, 0, 1) 44%,
+              rgba(75, 71, 102, 1) 100%
+            );
           }
         `}
       </style>
